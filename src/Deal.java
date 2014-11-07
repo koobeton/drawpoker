fe8f015fb20d430131316ceac0c3d8e1f0f3a3f8
Baburin Sergey <sbasba777@hotmail.com>
2014-11-07 22:50:57 +0300

    Add hold hand

--- src/Deal.java
+++ src/Deal.java
@@ -1,25 +1,71 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class Deal {
 
-    private static final int CARDS_TO_DEAL = 5;
+    private static final int CARDS_IN_HAND = 5;
+    private static List<Card> deck;
+    private static List<Card> hand;
 
     public static void main(String... args) {
 
-        Deck deck = new Deck();
-        List<Card> hand;
+        newDeal();
 
-        deck.shuffle();
+        dealHand(CARDS_IN_HAND);
+        System.out.println(hand);
 
-        try {
-            while (deck.size() > 0) {
-                hand = deck.dealHand(CARDS_TO_DEAL);
-                hand.sort(null);
-                System.out.println(hand);
+        holdHand();
+        dealHand(CARDS_IN_HAND - hand.size());
+        System.out.println(hand);
+
+        //TODO check combinations
+
+        deck.sort(null);
+        System.out.printf("Remaining cards (%d):%n%s%n", deck.size(), deck);
+    }
+
+    private static void newDeal() {
+
+        deck = new ArrayList<>();
+        hand = new ArrayList<>();
+        for (Suit suit : Suit.values()) {
+            for (Rank rank : Rank.values()) {
+                deck.add(new Card(rank, suit));
             }
-        } catch (IllegalArgumentException e) {
-            System.out.println(e.getMessage());
-            System.out.printf("Remaining cards:%n%s%n", deck);
         }
+        Collections.shuffle(deck);
+    }
+
+    private static void dealHand(int cardToDeal) {
+
+        hand.addAll(Deck.deal(deck, cardToDeal));
+        hand.sort(null);
+    }
+
+    private static void holdHand() {
+
+        BufferedReader reader =
+                new BufferedReader(new InputStreamReader(System.in));
+        String input;
+        List<Card> hold = new ArrayList<>();
+
+        System.out.print("Hold: ");
+        try {
+            input = reader.readLine().replaceAll("[^1-5]", "");
+            for (char c : input.toCharArray()) {
+                int i = Integer.parseInt(Character.toString(c));
+                if (!hold.contains(hand.get(i - 1))) {
+                    hold.add(hand.get(i - 1));
+                }
+                if (hold.size() == 5) break;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        hand = hold;
     }
 }
--- src/Deck.java
+++ src/Deck.java
@@ -1,29 +1,16 @@
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
-class Deck extends ArrayList<Card> {
+class Deck {
 
-    Deck() {
-        for (Suit suit : Suit.values()) {
-            for (Rank rank : Rank.values()) {
-                this.add(new Card(rank, suit));
-            }
-        }
-    }
+    static <E> List<E> deal(List<E> deck, int cardsToDeal) {
 
-    void shuffle() {
-        Collections.shuffle(this);
-    }
-
-    List<Card> dealHand(int cardsToDeal) {
-
-        int deckSize = this.size();
+        int deckSize = deck.size();
         if (deckSize < cardsToDeal) {
             throw new IllegalArgumentException("Not enough cards");
         }
-        List<Card> handView = this.subList(deckSize - cardsToDeal, deckSize);
-        List<Card> hand = new ArrayList<>(handView);
+        List<E> handView = deck.subList(deckSize - cardsToDeal, deckSize);
+        List<E> hand = new ArrayList<>(handView);
         handView.clear();
         return hand;
     }
